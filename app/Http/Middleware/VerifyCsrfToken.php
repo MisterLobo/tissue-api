d8eec17748a12989a15a040a7b1ce3b7cb9b59c2
Al Gae <algae1234567890@gmail.com>
2020-06-04 16:42:00 +0800

    add login providers to csrf except list

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends BaseVerifier
      * @var array
      */
     protected $except = [
-        //
+        '/sociallogin/google','/sociallogin/facebook','/sociallogin/github','/sociallogin/twitter','/sociallogin/vkontakte'
     ];
 }
