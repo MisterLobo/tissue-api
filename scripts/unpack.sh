58f1ea5dcdb982021d788fa3758d5f430ed8236c
Al Gae <algae1234567890@gmail.com>
2020-06-08 22:18:59 +0800

    add unpack script

---
+++ scripts/unpack.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+chmod +x ./scripts/decrypt.sh
+chmod +x ./scripts/xtar.sh
+./scripts/decrypt.sh "$1" vault.gpg vault.tar.gz
+./scripts/xtar.sh vault.tar.gz
+
+cp ./scripts/decrypt.sh vault
+
+cd vault || exit
+
+./decrypt.sh "$1" "$2".gpg "$2"
+./decrypt.sh "$1" cienv.gpg cienv
+
+./decrypt.sh "$1" "$3" ftpenv
+
+cp devenv ../
+cp cienv ../
+cp ftpenv ../
+
+cd ..
+
+rm vault.tar.gz
+rm -rf vault/
