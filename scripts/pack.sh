508e0dea26836388ef881ea860725318e82efca5
Al Gae <algae1234567890@gmail.com>
2020-06-08 22:19:19 +0800

    update pack script

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -9,6 +9,10 @@ cp demoenv vault/
 cp env vault/
 cp cienv vault/
 
+cp ciftpenv vault/
+cp ciftpenv_dev vault/
+cp ciftpenv_demo vault/
+
 cp ./scripts/encrypt.sh vault
 
 cd vault || exit
@@ -18,16 +22,24 @@ cd vault || exit
 ./encrypt.sh "$1" env
 ./encrypt.sh "$1" cienv
 
+./encrypt.sh "$1" ciftpenv
+./encrypt.sh "$1" ciftpenv_dev
+./encrypt.sh "$1" ciftpenv_demo
+
 rm ./encrypt.sh
 rm ./devenv
 rm ./demoenv
 rm ./env
 rm ./cienv
+rm ./ciftpenv
+rm ./ciftpenv_dev
+rm ./ciftpenv_demo
 
 cd ..
 
 ./scripts/ctar.sh vault.tar.gz vault/
 ./scripts/encrypt.sh "$1" vault.tar.gz
+mv vault.tar.gz.gpg vault.gpg
 
 rm vault.tar.gz
 rm -rf vault/
