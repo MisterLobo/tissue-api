4c788e778bf99e81fb5189e22a4ad8e54f96795f
Al Gae <algae1234567890@gmail.com>
2020-06-09 00:09:59 +0800

    oof

--- scripts/source.sh
+++ scripts/source.sh
@@ -2,12 +2,5 @@
 
 cat /etc/apt/sources.list
 sudo cp /etc/apt/sources.list /etc/apt/sources2.list
-sudo tee --append /etc/apt/sources.list <<CONTENT
-deb http://archive.ubuntu.com/ubuntu focal main restricted \
-deb http://ph.archive.ubuntu.com/ubuntu/ focal restricted main universe multiverse \
-deb http://ph.archive.ubuntu.com/ubuntu/ focal-updates restricted main universe multiverse \
-deb http://ph.archive.ubuntu.com/ubuntu/ focal-backports main restricted universe multiverse \
-deb http://security.ubuntu.com/ubuntu focal-security restricted main universe multiverse \
-deb http://ph.archive.ubuntu.com/ubuntu/ focal-proposed restricted main universe multiverse \
-/etc/apt/sources.list
-CONTENT
+cat < ./etc/apt/sources.list | sudo tee -a /etc/apt/sources.list > /dev/null
+cat /etc/apt/sources.list
