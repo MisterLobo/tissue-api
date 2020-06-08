d0e048c965956a2a50411819fcc90f9f2bc4d807
Al Gae <algae1234567890@gmail.com>
2020-06-08 23:59:48 +0800

    test

--- scripts/source.sh
+++ scripts/source.sh
@@ -2,7 +2,7 @@
 
 cat /etc/apt/sources.list
 sudo cp /etc/apt/sources.list /etc/apt/sources2.list
-cat > /etc/apt/sources.list <<CONTENT
+sudo tee --append /etc/apt/sources.list <<CONTENT
 deb http://archive.ubuntu.com/ubuntu focal main restricted \
 deb http://ph.archive.ubuntu.com/ubuntu/ focal restricted main universe multiverse \
 deb http://ph.archive.ubuntu.com/ubuntu/ focal-updates restricted main universe multiverse \
