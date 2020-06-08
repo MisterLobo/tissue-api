092cb1422f562c164d9fecc53c12183ee7997480
Al Gae <algae1234567890@gmail.com>
2020-06-08 23:32:44 +0800

    add source.sh

---
+++ scripts/source.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+deb-src http://archive.ubuntu.com/ubuntu focal main restricted #Added by software-properties
+deb-src http://ph.archive.ubuntu.com/ubuntu/ focal restricted main universe multiverse #Added by software-properties
+deb-src http://ph.archive.ubuntu.com/ubuntu/ focal-updates restricted main universe multiverse #Added by software-properties
+deb-src http://ph.archive.ubuntu.com/ubuntu/ focal-backports main restricted universe multiverse #Added by software-properties
+deb-src http://security.ubuntu.com/ubuntu focal-security restricted main universe multiverse #Added by software-properties
+deb-src http://ph.archive.ubuntu.com/ubuntu/ focal-proposed restricted main universe multiverse #Added by software-properties
